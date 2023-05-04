67879b4220d4f36816326fadfd8dbbd9ba855dae
tejashri42@gmail.com <tejashri42@gmail.com>
2023-05-04 21:08:34 +0530

    Created link tag

--- src/Cointainer/HomePAge/HomePage.js
+++ src/Cointainer/HomePAge/HomePage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+    <div>
+        <Link to='/product'>
+        <h2 style={{cursor:"pointer"}}>Go To Product PAge</h2>
+        </Link></div>
+    </div>
   )
 }
 
